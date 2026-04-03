97e82209f9d6d1dc83080363f23d1032ee434cfe
Alehandro <makedonskij368@gmail.com>
2026-04-03 11:30:07 +0300

    fix: remove unused engine imt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from app.database import get_db, 
+from app.database import get_db
 from app.models import Book
 
 
